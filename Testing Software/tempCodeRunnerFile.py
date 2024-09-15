d07b24b775f1171971dd82c8503febc3edcda6e9
RUDRANSH <tiwarirudransh2003@gmail.com>
2024-09-16 01:02:02 +0530

    NEW UPDATE

--- Testing Software/tempCodeRunnerFile.py
+++ Testing Software/tempCodeRunnerFile.py
@@ -1,15 +1,7 @@
-  # x = np.arange(10)
-    # y = np.zeros_like(x)  # Initial empty data
+def plot_live_data(ax, canvas, ylabel):
+#     '''Plot live data on the graph after launch using animation'''
+#     x = np.arange(10)
+#     y = np.zeros_like(x)  # Initial empty data
 
-    # line, = ax.plot(x, y, linestyle='-', color='cyan', linewidth=2)
-    # fig = ax.get_figure()
-
-    # def update(frame):
-    #     new_ydata = np.random.random(10) * (10 if ylabel == 'Acceleration' else 50)
-    #     line.set_ydata(new_ydata)
-    #     ax.relim()
-    #     ax.autoscale_view()
-    #     canvas.draw()
-
-    # ani = FuncAnimation(fig, update, interval=1000)
-    # return ani
+#     line, = ax.plot(x, y, linestyle='-', color='cyan', linewidth=2)
+#     fig = ax.get_figure()
